981944e6bf777e7053c93c06d3c9213a42522d6b
dgran <dgranh@gmail.com>
2021-09-21 03:35:57 +0200

    test overflow race menu

--- resources/views/tournament/dashboard.blade.php
+++ resources/views/tournament/dashboard.blade.php
@@ -2,7 +2,7 @@
     @include('tournament.partials.header', ['activeTab' => 'Dashboard', 'color' => 'edblue'])
 
     <x-container>
-
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur et amet omnis quisquam. Voluptas, impedit quam aliquid debitis vero, accusantium eaque quos dolore alias quidem accusamus perspiciatis neque minus provident.
 
     </x-container>
 </div>
