2703d51daf3f51296e4b2d38c92272cd79d8680c
dgran <dgranh@gmail.com>
2021-09-21 03:38:08 +0200

    test overflow race menu

--- resources/views/tournament/normative.blade.php
+++ resources/views/tournament/normative.blade.php
@@ -6,7 +6,7 @@
         <h4 class="mt-8 | font-semibold | font-fjalla | uppercase | text-xl md:text-2xl | tracking-wider | text-title-color dark:text-dt-title-color">Normativa</h4>
 
         <section class="mt-4 mb-6 md:mt-6 md:mb-8">
-            <embed src="{{ asset('tes.pdf') }}" type="application/pdf" height="900" class="w-full rounded-lg" />
+            {{-- <embed src="{{ asset('tes.pdf') }}" type="application/pdf" height="900" class="w-full rounded-lg" /> --}}
         </section>
 
     </x-container>
