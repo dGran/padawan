72b1130d3f85f995ae44ff863a6a9f822e8e6035
David Gran <dgranh@gmail.com>
2021-10-15 16:03:32 +0200

    working in create eteam

--- resources/views/eteams/create/confirmation-modal.blade.php
+++ resources/views/eteams/create/confirmation-modal.blade.php
@@ -1,19 +1,18 @@
 <div class="relative | bg-white dark:bg-dt-dark p-4 sm:py-8">
     <div class="flex flex-col items-center">
-    	<h4 class="text-normal md:text-base font-semibold">
-    		Confirmación
-    	</h4>
-    	<p class="pt-3">
-        	Como fundador del equipo, tendrás acceso al panel de administración del equipo donde podrás editar toda la información.
-        	Se podrán editar todos los datos desde el panel de administración del equipo.
+        <p class="pt-4 sm:pt-0 text-title-color dark:text-dt-title-color">
+            ¿Deseas crear el equipo?
+        </p>
+    	<p class="py-4 | text-center | text-xxs md:text-xs | text-text-light-color dark:text-dt-text-light-color">
+        	Podrás agregar o modificar cualquier dato posteriormete desde el panel de administración del equipo.
     	</p>
 
-    <div class="mt-6 flex items-center justify-between space-x-4">
-    	<x-button wire:click="$emit('closeModal')">
-    		Cancelar
-    	</x-button>
+    <div class="mt-2.5 | w-full | flex items-center justify-between space-x-4">
+    	<button class="px-4 py-2 | bg-white dark:bg-dt-dark | rounded | font-semibold text-sm text-title-color dark:text-dt-title-color | hover:bg-border-color dark:hover:bg-dt-border-color | focus:outline-none focus:bg-border-color dark:focus:bg-dt-border-color | transition ease-in-out duration-75 | select-none | cursor-pointer" wire:click="$emit('closeModal')">
+    		No, cancelar
+    	</button>
 	    <x-button wire:click="store">
-	        {{ __('Crear equipo') }}
+	        {{ __('Sí, crear equipo') }}
 	    </x-button>
     </div>
 
