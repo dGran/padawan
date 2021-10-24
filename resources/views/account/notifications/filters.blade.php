edb304eb2e616be9ac1acc89c3fc8514bd3cb190
dgran <dgranh@gmail.com>
2021-10-24 23:27:42 +0200

    working in notifications view

--- resources/views/account/notifications/filters.blade.php
+++ resources/views/account/notifications/filters.blade.php
@@ -1,7 +1,7 @@
 @if ($filterText || $filterUnread)
 	<div class="px-3 mb-1.5 | text-edyellow-600 dark:text-edyellow-400">
 		@if ($filterUnread)
-			<span class="text-xxs md:text-xs | font-medium">
+			<span class="text-xxs md:text-xs | font-semibold">
 				Filtro: No leidos
 			</span>
 		@endif
