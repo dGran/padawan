21b796f0831ca63050ecaa7e0170d35b5fa6dce0
dgran <dgranh@gmail.com>
2022-07-02 04:11:01 +0200

    [IMP] menu auth header - change border user icon in dark mode

--- resources/views/layouts/partials/app/navigation/dropdown-auth.blade.php
+++ resources/views/layouts/partials/app/navigation/dropdown-auth.blade.php
@@ -1,7 +1,7 @@
 <x-dropdown align="right" width="max" class="relative">
     <x-slot name="trigger">
         <button
-            class="border-2 border-border-color dark:border-dt-border-color | rounded-full | hover:border-edblue-400 dark:hover:border-edblue-300 | focus:outline-none focus:border-edblue-400 dark:focus:border-edblue-300 | text-dt-text-color dark:text-text-color | hover:text-dt-title-color dark:hover:text-title-color | focus:outline-none focus:text-dt-title-color dark:focus:text-title-color">
+            class="border-2 border-border-color dark:border-edgray-600 | rounded-full | hover:border-edblue-400 dark:hover:border-edblue-300 | focus:outline-none focus:border-edblue-400 dark:focus:border-edblue-300 | text-dt-text-color dark:text-text-color | hover:text-dt-title-color dark:hover:text-title-color | focus:outline-none focus:text-dt-title-color dark:focus:text-title-color">
             <div
                 class="flex items-center text-sm | rounded-full | h-8 w-8 md:h-9 md:w-9 m-0.5 | bg-text-color dark:bg-dt-text-color">
                 <img src="{{ auth()->user()->getAvatarUrl() }}" alt="{{ auth()->user() . " avatar" }}"
